40feaa87a2865bff794c1f5dddc2046b653dd440
Burak Kahramanöz <burakkahramanoz123@gmail.com>
2026-01-05 19:22:57 +0300

    Veritabanı Arayüzü ve Sahte Veri Katmanı (Mock Data) Oluşturuldu
    ## Veritabanı Arayüzü ve Mock Data Entegrasyonu
    
    Projenin veri yönetim katmanını daha esnek ve test edilebilir hale getirmek için aşağıdaki geliştirmeler yapılmıştır:
    
    * **IVeriTabani (Interface):** Veritabanı işlemlerini standartlaştırmak için bir sözleşme yapısı kuruldu. Bu sayede ileride gerçek bir SQL veritabanına geçiş yaparken kodun diğer kısımlarını değiştirmemize gerek kalmayacak.
    * **SahteVeriTabani (Mock Class):** Interface'i implemente eden ve verileri bellekte (`ArrayList`) tutan sınıf oluşturuldu.
    * **Hazır Veriler:** Uygulama açıldığında testlerin kolayca yapılabilmesi için Constructor içerisinde örnek Halı Saha verileri (Yıldız Arena, Şampiyonlar vb.) eklendi.
    * **Metodlar:** Saha ekleme/getirme ve Rezervasyon kaydetme/getirme fonksiyonları işlevsel hale getirildi.

---
+++ SahteVeriTabanı.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Interface'i implemente eden sınıf (Verileri şimdilik listede tutuyoruz)
+public class SahteVeriTabani implements IVeriTabani {
+    private List<HaliSaha> sahalar = new ArrayList<>();
+    private List<Rezervasyon> rezervasyonlar = new ArrayList<>();
+
+    public SahteVeriTabani() {
+        // Hazır veriler (Constructor'da yüklenir)
+        sahalar.add(new HaliSaha("Yıldız Arena", "Merkez", 1400));
+        sahalar.add(new HaliSaha("Şampiyonlar Halı Saha", "Kampüs", 1250));
+        sahalar.add(new HaliSaha("Olimpik Tesisler", "Sanayi", 1600));
+    }
+
+    @Override
+    public void sahaEkle(HaliSaha saha) {
+        sahalar.add(saha);
+    }
+
+    @Override
+    public List<HaliSaha> sahalariGetir() {
+        return sahalar;
+    }
+
+    @Override
+    public void rezervasyonKaydet(Rezervasyon r) {
+        rezervasyonlar.add(r);
+    }
+
+    @Override
+    public List<Rezervasyon> rezervasyonlariGetir() {
+        return rezervasyonlar;
+    }
+}
