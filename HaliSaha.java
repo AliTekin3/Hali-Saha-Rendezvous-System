4cb3c2b1159b99256e41c62be58a275ed82c5ce4
Burak Kahramanöz <burakkahramanoz123@gmail.com>
2026-01-05 19:52:43 +0300

    Add ID field and constructor overload to HaliSaha

--- HaliSaha.java
+++ HaliSaha.java
@@ -1,16 +1,24 @@
 public class HaliSaha {
+    private int id; 
     private String isim;
     private String konum;
     private double ucret;
 
-    public HaliSaha(String isim, String konum, double ucret) {
+    public HaliSaha(int id, String isim, String konum, double ucret) {
+        this.id = id;
         this.isim = isim;
         this.konum = konum;
         this.ucret = ucret;
     }
 
+    public HaliSaha(String isim, String konum, double ucret) {
+        this(0, isim, konum, ucret);
+    }
+
+    public int getId() { return id; }
     public String getIsim() { return isim; }
-    // toString metodunu override ettik ki arayüzde nesne adresi değil ismi görünsün
+    public double getSaatlikUcret() { return ucret; }
+
     @Override
     public String toString() { return isim + " (" + ucret + " TL)"; }
 }
