5992effd733788a3cfc5187d06a3d2cb07ac7538
Burak Kahramanöz <burakkahramanoz123@gmail.com>
2026-01-05 19:17:10 +0300

    Veritabanı arayüzü (IVeriTabani) ve metod imzaları oluşturuldu
    Veritabanı bağımlılığını soyutlamak amacıyla IVeriTabani interface'i projeye eklendi. Şu işlemler için gerekli metod tanımları yapıldı:
    
    Halı saha ekleme ve listeleme (HaliSaha)
    
    Rezervasyon oluşturma ve görüntüleme (Rezervasyon)
    
    Bu arayüz, Mock ve gerçek veritabanı sınıfları için bir sözleşme görevi görecektir.

--- VeriTabani.java
+++ VeriTabani.java
@@ -1,9 +1,10 @@
 import java.util.List;
 
-// Sözleşme sınıfımız. Veritabanı olacak sınıfın hangi metodlara sahip olması gerektiğini söyler.
+//Sözleşme sınıfımız. Veritabanı olacak sınıfın hangi metodlara sahip olması gerektiğini söyler.
 public interface IVeriTabani {
     void sahaEkle(HaliSaha saha);
     List<HaliSaha> sahalariGetir();
     void rezervasyonKaydet(Rezervasyon r);
     List<Rezervasyon> rezervasyonlariGetir();
+
 }
